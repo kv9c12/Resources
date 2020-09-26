b92d6a7f733ac17c661e74809d9a4a8b193c64ed
kv9c12 <varun9c12@gmail.com>
2020-09-26 23:53:00 +0530

    Update Node.js

--- Node.js
+++ Node.js
@@ -1,2 +1,5 @@
 File setup settings
 https://stackoverflow.com/questions/28782656/how-to-run-node-js-app-with-es6-features-enabled
+
+No Babel
+https://blog.logrocket.com/you-dont-need-babel-with-node/
